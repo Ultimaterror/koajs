805bfddef3a531f97da8f5d936ead5ec952a2c3e
A L <ultimaterror@laposte.net>
2021-10-06 16:20:09 +0200

    W Name change

--- front/src/App.js
+++ front/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Categories from "./Components/Categories";
+import Collection from "./Components/Collection";
 
 
 function App() {
   return (
     <Router>
-      <Route exact path="/get-db-collection/:collection" component={Categories} />
+      <Route exact path="/get-db-collection/:collection" component={Collection} />
     </Router>
   );
 }
